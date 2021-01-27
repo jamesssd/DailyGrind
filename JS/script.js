1226627e8627d5b66775298e8b14fbf61780b105
James Dizon <dizonjerlin@gmail.com>
2021-01-26 19:07:10 -0800

    updated files

--- JS/script.js
+++ JS/script.js
@@ -1,44 +1,131 @@
-// Day of Week
-// Coffe Name
-// Coffee Pic
-// Alt Tag
-// Coffee Description
-// Color of Cofee
+/*
+Day of week - day
+Coffee Name - name
+Coffee Pic - pic
+Alt tag - alt
+Coffee Description - desc
+Color for coffee - color
+*/
 
 
 
 let myDate = new Date();
-let myDay = myDate.getDate();
-let today = " ";
-let coffee = " ";
+let myDay = "";
+let today = "";
+let coffee = "";
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
 
+
+if(urlParams.has('day')){ //use query string
+    myDay= urlParams.get('day');
+    myDay= parseInt(myDay);
+}else{//use date object
+    myDay = myDate.getDay();
+}
+
+function coffeeTemplate(coffee){
+    let myReturn = "";
+
+
+    myReturn=`
+    <p>
+    <img src="${coffee.pic}" alt="Our ${coffee.alt}" id="coffee" />
+    <strong id= "coffee-highlight" class="feature">${coffee.day}'s Coffee Special:</strong> ${coffee.day}'s daily coffee special is <strong>${coffee.name}</strong>,
+    ${coffee.desc}
+    </p>
+    `;
+ 
+    return myReturn;
+
+}
 
 switch(myDay){
     case 1:
         today = "Monday";
-
-        coffee={
-            color: "pink",
+        coffee = {
+            color: "Pink",
             name: "Bubble Tea",
-            pic: "images/bubble-tea.jpg",
-            alt: "A pic of a Bubble Tea",
+            pic: "Images/bubble-tea.jpg",
+            alt: "A pic of bubble tea",
             day: "Monday",
-            desc: "I love Bubble Tea!" 
+            desc: `I love bubble tea!`
         };
-    break;
-
+        break;
+    case 2:
+        today = "Tuesday";
+        coffee = {
+            color: "yellow",
+            name: "Caramel Latte",
+            pic: "Images/caramel-latte.jpg",
+            alt: "A pic of caramel-latte",
+            day: "Tuesday",
+            desc: `Great scott!, it's Caramel latte day!`
+        };
+        break;
     case 3:
-        today = "Wednesday";
-
-        coffee={
+        today = "Wenesday";
+        coffee = {
             color: "brown",
             name: "Drip",
-            pic: "images/drip.png",
-            alt: "A pic of a drip coffee",
+            pic: "Images/drip.jpg",
+            alt: "A pic of drip Coffee",
             day: "Wednesday",
-            desc: "Some days you just need the drip!" 
+            desc: `Some days you just need the drip!`
         };
-    break;
+        break;
+    
+    case 4:
+        today = "Thursday";
+        coffee = {
+            color: "Pink",
+            name: "Bubble Tea",
+            pic: "Images/bubble-tea.jpg",
+            alt: "A pic of bubble tea",
+            day: "Monday",
+            desc: `Some days you just need the bubble tea!`
+        };
+        break;
+    case 5:
+        today = "Friday";
+        coffee = {
+            color: "yellow",
+            name: "Caramel Latte",
+            pic: "Images/caramel-latte.jpg",
+            alt: "A pic of caramel-latte",
+            desc: `Great scott!, it's Caramel latte day!`
+        };
+        break;
+    case 6:
+        today = "Saturday";
+        coffee = {
+            color: "brown",
+            name: "Drip",
+            pic: "Images/drip.jpg",
+            alt: "A pic of drip Coffee",
+            desc: `Some days you just need the drip!`
+        };
+        break;
+    
+    case 7:
+        today = "Sunday";
+        coffee = {
+            color: "brown",
+            name: "Drip",
+            pic: "Images/drip.jpg",
+            alt: "A pic of drip Coffee",
+            desc: `Some days you just need the drip!`
+        };
+        break;
+        
 }
 
-alert("Hi it's " + today );
+console.log(coffee);
+
+document.getElementById("coffee-output").innerhtml = coffeeTemplate(coffee);
+
+document.getElementById("HTML")[0].style.backgroundColor = coffee.color;
+
+document.getElementById("coffee-highlight").style.color = coffee.color;
+
+alert("Hi, it's " + today);
